a7df173ac8636b738feb20a95073de8a3c2efb1f
Florian <florian.delconte@protonmail.com>
2019-04-03 12:57:59 +0200

    ajout de la detection de Haris

--- segmentation_mire/hello_wolrd.m
+++ segmentation_mire/hello_wolrd.m
@@ -1,1 +1,10 @@
-qsjsnsnsk
+name='D01-L1-6.JPG';
+I=imread(name);
+fig=figure('Name','Segmentation de mire');
+subplot(1,2,1);
+
+corners = detectHarrisFeatures(I);
+
+imshow(I); hold on;
+plot(corners.selectStrongest(300));
+truesize(fig);
