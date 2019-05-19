e351ecd59a398162290201b1eba9db5fcfb03e38
Florian <florian.delconte@protonmail.com>
2019-05-19 20:26:55 +0200

    test DGtal

--- outils/test_DGtal.cpp
+++ outils/test_DGtal.cpp
@@ -1,49 +1,53 @@
-#include "DGtal/base/Common.h"
-#include "DGtal/helpers/StdDefs.h"
+#include <iostream>
 #include "ConfigExamples.h"
-#include "DGtal/io/readers/PGMReader.h"
-#include "DGtal/images/imagesSetsUtils/SetFromImage.h"
-#include "DGtal/topology/helpers/Surfaces.h"
-#include "DGtal/geometry/curves/FreemanChain.h"
+#include "DGtal/helpers/StdDefs.h"
+#include "DGtal/base/Common.h"
+#include "DGtal/geometry/curves/AlphaThickSegmentComputer.h"
 #include "DGtal/io/boards/Board2D.h"
-#include "DGtal/io/colormaps/GradientColorMap.h"
-#include "DGtal/io/Color.h"
-
+#include "DGtal/io/readers/PointListReader.h"
+using namespace std;
 using namespace DGtal;
-int main()
+int main(  )
 {
-  typedef DGtal::ImageContainerBySTLVector< DGtal::Z2i::Domain, unsigned char> Image;
-  std::string filename =  examplesPath + "samples/circleR10modif.pgm";
-  Image image = DGtal::PGMReader<Image>::importPGM(filename);
-
-  Z2i::KSpace ks;
-  ks.init( image.domain().lowerBound(), image.domain().upperBound(), true );
-  Z2i::DigitalSet set2d (image.domain());
-  SetFromImage<Z2i::DigitalSet>::append<Image>(set2d, image, 1, 255);
+  trace.beginBlock ( "Example exampleAlphaThickSegment" );
+  typedef FreemanChain<Z2i::Space::Integer>::ConstIterator FCConstIterator;
+  typedef  AlphaThickSegmentComputer< Z2i::Point, FCConstIterator > AlphaThickSegmentComputer2D;
   Board2D aBoard;
-  aBoard << set2d;
-  aBoard << image.domain();
-  SurfelAdjacency<2> sAdj( true );
-  std::vector< std::vector< Z2i::Point >  >  vectContoursBdryPointels;
-  Surfaces<Z2i::KSpace>::extractAllPointContours4C( vectContoursBdryPointels,
-                ks, set2d, sAdj );
-
-  GradientColorMap<int> cmap_grad( 0, (const int)vectContoursBdryPointels.size() );
-  cmap_grad.addColor( Color( 50, 50, 255 ) );
-  cmap_grad.addColor( Color( 255, 0, 0 ) );
-  cmap_grad.addColor( Color( 255, 255, 10 ) );
-  cmap_grad.addColor( Color( 25, 255, 255 ) );
-  cmap_grad.addColor( Color( 255, 25, 255 ) );
-  cmap_grad.addColor( Color( 25, 25, 25 ) );
-
-  for(unsigned int i=0; i<vectContoursBdryPointels.size(); i++){
-    //  Constructing and displaying FreemanChains from contours.
-    FreemanChain<Z2i::Integer> fc (vectContoursBdryPointels.at(i));
-    aBoard << SetMode( fc.className(), "InterGrid" );
-    aBoard<< CustomStyle( fc.className(),
-            new CustomColors(  cmap_grad(i),  Color::None ) );
-    aBoard << fc;
+  // Reading input contour
+  std::string freemanChainFilename = examplesPath + "samples/contourS.fc";
+  fstream fst;
+  fst.open (freemanChainFilename.c_str(), ios::in);
+  FreemanChain<Z2i::Space::Integer> fc(fst);
+  fst.close();
+  aBoard << fc;
+  //construction of an AlphaThickSegmentComputer2D from the freemanchain iterator
+  AlphaThickSegmentComputer2D anAlphaSegment(15), anAlphaSegment2(5), anAlphaSegment3(2);
+  anAlphaSegment.init(fc.begin());
+  while (anAlphaSegment.end() != fc.end() &&
+         anAlphaSegment.extendFront()) {
   }
-  aBoard.saveEPS("freemanChainFromImage.eps");
+  aBoard << anAlphaSegment;
+  // Example of thickness definition change: usin the euclidean thickness definition.
+  AlphaThickSegmentComputer2D anAlphaSegment2Eucl(5, functions::Hull2D::EuclideanThickness);
+  anAlphaSegment2Eucl.init(fc.begin());
+  while (anAlphaSegment2Eucl.end() != fc.end() &&
+         anAlphaSegment2Eucl.extendFront()) {
+  }
+  aBoard << CustomStyle( anAlphaSegment2Eucl.className(),
+                         new CustomColors( DGtal::Color(20, 250, 255), DGtal::Color::None ) );
+  aBoard << anAlphaSegment2Eucl;
+  anAlphaSegment2.init(fc.begin());
+  while (anAlphaSegment2.end() != fc.end() && anAlphaSegment2.extendFront()) {
+  }
+  aBoard  << CustomStyle( anAlphaSegment2.className(), new CustomColors( DGtal::Color::Blue, DGtal::Color::None ) );
+  aBoard << anAlphaSegment2;
+  FCConstIterator fcIt = fc.begin();
+  while (anAlphaSegment3.extendFront(*fcIt)) {
+    fcIt++;
+  }
+  aBoard  << CustomStyle( anAlphaSegment3.className(), new CustomColors( DGtal::Color::Green, DGtal::Color::None ) );
+  aBoard << anAlphaSegment3;
+  aBoard.saveEPS("exampleAlphaThickSegment.eps");
+  trace.endBlock();
   return 0;
 }
