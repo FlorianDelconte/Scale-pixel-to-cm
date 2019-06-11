f607bc34b485fb1313e3e53468340ead59220c71
Florian <florian.delconte@protonmail.com>
2019-06-11 17:28:58 +0200

    -

--- segmentation_mire/canny_repo.m
+++ segmentation_mire/canny_repo.m
@@ -1,7 +1,8 @@
 clear all
 
-path_source='../DATA/PNG/truth_ground/img/normal_size/';
-path_dest='../DATA/PNG/sgm_mire/';
+path_source='../DATA/PNG/truth_ground/img/256_256/';
+%path_dest='../DATA/PNG/sgm_mire/';
+path_dest='../DATA/PNG/truth_ground/img/test/';
 %obtient la liste des images 
 filelist_sgm=[dir(strcat(path_source,'*.png'));dir(strcat(path_source,'*.PNG'))];
 nfiles = length(filelist_sgm);
@@ -10,7 +11,8 @@ fig=figure('Name','Segmentation de mire');
 for i = 1:nfiles
     tic;
     path_name=strcat(strcat(path_source, '/'), filelist_sgm(i).name)
-    path_name_write=strcat(strcat(path_dest, '/img_canny/S/'), filelist_sgm(i).name)
+    %path_name_write=strcat(strcat(path_dest, '/img_canny/S/'), filelist_sgm(i).name)
+    path_name_write=strcat(strcat(path_dest, '/R/'), filelist_sgm(i).name)
     I_RGB=imread(path_name);
     I_RGB = imgaussfilt(I_RGB);
     [I_RGB,R,G,B,I_HSV,H,S,V] = create_composante(I_RGB);   
