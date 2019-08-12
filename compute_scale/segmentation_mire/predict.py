7f19f67ff1dabb914c361db131ebb421726cd041
DoozKawa <doozkawa@allegro2.loria.fr>
2019-08-12 14:37:24 +0200

    remise fonctionnel du code

--- compute_scale/segmentation_mire/predict.py
+++ compute_scale/segmentation_mire/predict.py
@@ -40,22 +40,22 @@ else:
 for path in os.listdir(dir_input):
 	if path == ".DS_Store": continue
 	file 	= "{}/{}".format(dir_input, path)
-	if file=="/home/doozkawa/Bureau/stage_Tai/compute_scale/segmentation_mire/../../DATA/PNG/test/mire/512_512/input/huawei_E074B_5.png" :
-		print(file)
-		img 	= io.imread(file, plugin='matplotlib')
+
+	print(file)
+	img 	= io.imread(file, plugin='matplotlib')
 
 
-		h=img.shape[0]
-		w=img.shape[1]
+	h=img.shape[0]
+	w=img.shape[1]
 
-		print(img.shape)
-		img 	= cv2.resize(img, (model.height, model.width), interpolation =cv2.INTER_AREA)
-		img 	= np.array([img])
-		img 	= np.reshape(img, [1, model.height, model.width, model.channels])
-		px 		= net.predict(img, verbose=1)
-		#resize de la sortie a la taille de l'image de base
-		segmentation	= px[0,:,:,0]
-		segmentation 	= cv2.resize(segmentation, (h, w), interpolation =cv2.INTER_NEAREST)
-		segmentation = (segmentation * 255).astype(np.uint8)
-		segmentation	=Image.fromarray(segmentation,"L")
-		segmentation.save('{}/{}'.format(dir_output, path))
+	print(img.shape)
+	img 	= cv2.resize(img, (model.height, model.width), interpolation =cv2.INTER_AREA)
+	img 	= np.array([img])
+	img 	= np.reshape(img, [1, model.height, model.width, model.channels])
+	px 		= net.predict(img, verbose=1)
+	#resize de la sortie a la taille de l'image de base
+	segmentation	= px[0,:,:,0]
+	segmentation 	= cv2.resize(segmentation, (h, w), interpolation =cv2.INTER_NEAREST)
+	segmentation = (segmentation * 255).astype(np.uint8)
+	segmentation	=Image.fromarray(segmentation,"L")
+	segmentation.save('{}/{}'.format(dir_output, path))
